1ba7fcde5cc91fce850d7833cee6f49d866e5056
onurilhancoban <onurilhanc@gmail.com>
2022-07-30 18:17:00 +0300

    Made CalculateGateSlice asynchronous.

--- Simulator/Simulator.cs
+++ Simulator/Simulator.cs
@@ -23,7 +23,7 @@ namespace Simulator
 
 
         //https://www.researchgate.net/publication/264003521_Efficient_algebraic_representation_of_quantum_circuits
-        public Matrix<Complex32> CalculateGateSlice(QuantumGate gate, int qubitIndex)
+        public async Task<Matrix<Complex32>> CalculateGateSliceAsync(QuantumGate gate, int qubitIndex)
         {
             if(qubitIndex < 1 || qubitIndex > qubitCount)
             {
@@ -41,8 +41,8 @@ namespace Simulator
             var controlList = gate.ControlList.OrderBy(s => s.Index).ToList(); //Order controls from most significant (q0) to least (qN-1)
             bool[] controlInitArray = Enumerable.Repeat(false, controlList.Count).ToArray();
 
-            var product = Matrix<Complex32>.Build.SparseIdentity(1); //Product of each term
-            var sum = Matrix<Complex32>.Build.Sparse((int)Math.Pow(2, qubitCount), (int)Math.Pow(2, qubitCount)); //the final sum of all terms
+            var product = await Task.Run(() => Matrix<Complex32>.Build.SparseIdentity(1)); //Product of each term
+            var sum = await Task.Run(() => Matrix<Complex32>.Build.Sparse((int)Math.Pow(2, qubitCount), (int)Math.Pow(2, qubitCount))); //the final sum of all terms
 
             //Loop through N terms
             for (int i = 0; i < controlList.Count; i++)
@@ -55,7 +55,7 @@ namespace Simulator
                     {
                         //Basically we perform an XOR on the init state and the type of the control to find out whether to use |0><0| or |1><1|
                         var theD = Convert.ToInt32(controlInitArray[j] != controlList[j].Type);
-                        product = product.KroneckerProduct(Helpers.D[theD]);
+                        product = await Task.Run(() => product.KroneckerProduct(Helpers.D[theD]));
 
                         //Initialize control if it isn't initialized already
                         if (controlInitArray[j] == false)
@@ -66,13 +66,13 @@ namespace Simulator
                     //If there aren't any controls, multiply by identity.
                     else
                     {
-                        product = product.KroneckerProduct(Helpers.Identity);
+                        product = await Task.Run(() => product.KroneckerProduct(Helpers.Identity));
                     }
                 }
                 //Add the product to the sum,
-                sum += product;
+                sum = await Task.Run(() => sum + product);
                 //And reset the product.
-                product = Matrix<Complex32>.Build.SparseIdentity(1);
+                product = await Task.Run(() => Matrix<Complex32>.Build.SparseIdentity(1));
             }
             //Loop through each qubit for the last term
             for(int k = 0; k < qubitCount; k++)
@@ -80,7 +80,7 @@ namespace Simulator
                 //If there is our gate on the qubit, apply the gate
                 if(qubitIndex == k)
                 {
-                    product = product.KroneckerProduct(gate.GateMatrix);
+                    product = await Task.Run(() => product.KroneckerProduct(gate.GateMatrix));
 
                     //This will prevent the gate from being applied multiple times
                     k += (gate.GateSize - 1);
@@ -90,14 +90,14 @@ namespace Simulator
                 {
                     //Apply the correct D depending on the control type.
                     var theD = Convert.ToInt32(!controlList[k].Type);
-                    product = product.KroneckerProduct(Helpers.D[theD]);
+                    product = await Task.Run(() => product.KroneckerProduct(Helpers.D[theD]));
                 }
                 else
                 {
-                    product = product.KroneckerProduct(Helpers.Identity);
+                    product = await Task.Run(() => product.KroneckerProduct(Helpers.Identity));
                 }
             }
-            sum += product;
+            sum = await Task.Run(() => sum + product);
 
             return sum;
         }
